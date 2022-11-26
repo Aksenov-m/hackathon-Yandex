25a7c24f4797621376b1fcd53103dd2785327354
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-11-26 18:55:29 +0400

    feat: create branch

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,11 @@
 import { h } from 'preact';
+import Vacancies from '../vacancies/Vacancies'
 
 const Main = () => (
 	<div class='main' id='main'>
-		<h1>Main</h1>
+    <h1>Main</h1>
 		<p>This is the Main component.</p>
+		<Vacancies />
 	</div>
 );
 
---
+++ src/components/vacancies/Vacancies.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+
+const Vacancies = () => (
+	<div class ="footer" id="footer">
+		<h1>Vacancies</h1>
+		<p>This is the Vacancies component.</p>
+	</div>
+);
+
+export default Vacancies;
