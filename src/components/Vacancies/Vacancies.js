95b2418d6ec5cbd1eb271e9894d83c95dc37adca
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-11-28 11:45:22 +0400

    feat: логика фильтрации

--- src/components/Vacancies/Vacancies.js
+++ src/components/Vacancies/Vacancies.js
@@ -7,24 +7,35 @@ const Vacancies = () => {
 
   const [filteredMaster, setFilteredMaster] = useState(VACANCY_CARDS_MASTER);
   const [filteredRewier, setFilteredRewier] = useState(VACANCY_CARDS_REWIEWER);
-  //const [notFound, setNotFound] = useState(false);
-  //const [mentor, setmentor] = useState(true);
-  //const [rewiew, setrewiew] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const [mentor, setmentor] = useState(true);
+  const [rewiew, setrewiew] = useState(false);
 
 
-    function vacancyFilter(argument){
+    function vacancyMasterFilter(argument){
       const NEW_VACANCY_CARDS_MASTER = [...VACANCY_CARDS_MASTER].filter(item => item.status === argument);
-      const NEW_VACANCY_CARDS__REWIEWER = [...VACANCY_CARDS_REWIEWER].filter(item => item.status === argument);
+      //const NEW_VACANCY_CARDS__REWIEWER = [...VACANCY_CARDS_REWIEWER].filter(item => item.status === argument);
       setFilteredMaster(NEW_VACANCY_CARDS_MASTER);
+      //setFilteredRewier(NEW_VACANCY_CARDS__REWIEWER)
+      setNotFound(false);
+      if(NEW_VACANCY_CARDS_MASTER.length === 0){
+        setNotFound(true);
+      }
+    }
+
+    function vacancyRewiewFilter(argument){
+      //const NEW_VACANCY_CARDS_MASTER = [...VACANCY_CARDS_MASTER].filter(item => item.status === argument);
+      const NEW_VACANCY_CARDS__REWIEWER = [...VACANCY_CARDS_REWIEWER].filter(item => item.status === argument);
+      //setFilteredMaster(NEW_VACANCY_CARDS_MASTER);
       setFilteredRewier(NEW_VACANCY_CARDS__REWIEWER)
-      //setNotFound(false);
-      //if(NEW_VACANCY_CARDS_MASTER.length === 0 || NEW_VACANCY_CARDS__REWIEWER === 0){
-      //  setNotFound(true);
-      //}
+      setNotFound(false);
+      if(NEW_VACANCY_CARDS__REWIEWER === 0){
+        setNotFound(true);
+      }
     }
 
 
-  /*function vacancyFilterProf1(){
+  function vacancyMentorFilter(){
     if (!mentor) {
       setmentor(true)
     } else {
@@ -32,13 +43,13 @@ const Vacancies = () => {
     }
   }
 
-  function vacancyFilterProf2(){
+  function vacancyRewiewFilter2(){
     if (!rewiew) {
       setrewiew(true)
     } else {
     setrewiew(false)
     }
-  }*/
+  }
 
   return (
 
@@ -46,23 +57,46 @@ const Vacancies = () => {
     <div class='vacancies__box'>
       <h1 class='vacancies__title'>Вакансии</h1>
       <div class='vacancies__button-box'>
-        <button class='vacancies__button vacancies__button_master'>Для наставников</button>
+        <button class='vacancies__button vacancies__button_master' onClick={vacancyMentorFilter}>Для наставников</button>
 
-        <VacancyNavbar handlefilterVacancy = {vacancyFilter} />
+        {mentor && <VacancyNavbar handlefilterVacancy = {vacancyMasterFilter} />}
 
-        <div class="vacancies__card-list">
-          {filteredMaster.map(card => (<VacancyCard key={card.id} text={card.text} />))}
+        {notFound ?
+
+        <div>
+          <p>Ничего не найдено</p>
         </div>
-        <button class='vacancies__more'>Ещё вакансии</button>
 
-        <button class='vacancies__button vacancies__button_rewiew'>Для ревьюеров</button>
+        :
 
-        <VacancyNavbar />
+        <>
+        <div class="vacancies__card-list">
+          {mentor && filteredMaster.map(card => (<VacancyCard key={card.id} text={card.text} />))}
+        </div>
+        {mentor && <button class='vacancies__more'>Ещё вакансии</button>}
+        </>
+        }
 
+        <button class='vacancies__button vacancies__button_rewiew' onClick={vacancyRewiewFilter2}>Для ревьюеров</button>
+
+        {rewiew && <VacancyNavbar handlefilterVacancy = {vacancyRewiewFilter} />}
+
+        {notFound ?
+
+        <div>
+          <p>Ничего не найдено</p>
+        </div>
+
+        :
+
+       <>
        <div class="vacancies__card-list">
-         {filteredRewier.map(card => (<VacancyCard key={card.id} text={card.text} />))}
+         {rewiew && filteredRewier.map(card => (<VacancyCard key={card.id} text={card.text} />))}
        </div>
-       <button class='vacancies__more'>Ещё вакансии</button>
+       {rewiew &&<button class='vacancies__more'>Ещё вакансии</button>}
+       </>
+       }
+
        </div>
        </div>
     </section>
