b3514a116baaa34d3f8689bbef139c13b5b6eac1
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-11-30 13:19:26 +0400

    исправить катины картинки, закомментировать небилдящийся код

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,18 +5,18 @@ import Navigation from "../Navigation/Navigation";
 import Logo from "../Logo/Logo";
 
 function Header() {
-  let bodyElement = document.getElementsByTagName("body")[0];
+  //let bodyElement = document.getElementsByTagName("body")[0];
   // Стейт, отвечающий за бургер
   const [isBurgerOpen, setBurgerOpen] = useState(false);
 
   function handleBurgeClick() {
     setBurgerOpen(true);
-    bodyElement.classList.add("lock");
+    //bodyElement.classList.add("lock");
   }
 
   function closeBurger() {
     setBurgerOpen(false);
-    bodyElement.classList.remove("lock");
+    //bodyElement.classList.remove("lock");
   }
 
   useEffect(() => {
